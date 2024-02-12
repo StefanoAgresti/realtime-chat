2348ede3110855ce520382553ae8c16cabaca2f5
Stefano Agresti <76478700+StefanoAgresti@users.noreply.github.com>
2024-02-12 11:32:12 +0100

    custom guards deleted

--- src/app/guards/is-logged-in.guard.ts
+++
@@ -1,8 +0,0 @@
-import { AuthService } from '../services/auth.service';
-import { inject } from '@angular/core';
-
-export const isLoggedInGuard = () => {
-  const auth = inject(AuthService);
-
-  return auth.userIsLoggedIn();
-};
