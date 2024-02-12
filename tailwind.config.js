e5ddf30c3a32f7fdd50452b5d133c2ed2e28a2e8
Stefano Agresti <76478700+StefanoAgresti@users.noreply.github.com>
2024-02-12 11:26:38 +0100

    custom maxheigth

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      maxHeight: {
+        160: "40rem",
+      },
+    },
   },
   plugins: [],
 };
